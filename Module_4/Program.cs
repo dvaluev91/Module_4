707f6a134bb9ff08d52564a84ef6ea35f78cb10e
dvaluev91 <dvaluev91@yandex.ru>
2024-03-03 21:38:14 +0300

    Добавьте файлы проекта.

---
+++ Module_4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Module_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myApples = 5;
+            var hisApples = 6;
+            var hisSpeares = 7;
+
+            var result = (myApples != hisSpeares) & (myApples < hisApples);
+            Console.WriteLine(result);
+        }
+    }
+}
