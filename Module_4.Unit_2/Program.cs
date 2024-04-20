35c0ddbbbcdab0d8da1f100545cf33eaf8391418
dvaluev91 <dvaluev91@yandex.ru>
2024-04-20 21:52:37 +0300

    Module4-master: Задание 4.1.18 работа с переключателем Switch

--- Module_4.Unit_2/Program.cs
+++ Module_4.Unit_2/Program.cs
@@ -84,6 +84,38 @@
 
                 Console.WriteLine("Your color is cyan!");
             }
+
+            //Задание 4.1.18
+
+            Console.WriteLine("Введите свой любимый цвет на английском языке с маленькой буквы");
+            var MyFavoriteColor = Console.ReadLine();
+            switch (MyFavoriteColor)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!"); 
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
         }
     }
 }
